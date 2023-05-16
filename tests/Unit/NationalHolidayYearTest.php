fbb692d55fbeb979c1640968161188b564f1895d
Tom 'Neal <tomborneman@livewallgroup.com>
2023-05-16 14:00:39 +0200

    feature: expand testWorkDays by validating that only the workdays are present in the array.

--- tests/Unit/NationalHolidayYearTest.php
+++ tests/Unit/NationalHolidayYearTest.php
@@ -17,5 +17,12 @@ class NationalHolidayYearTest extends TestCase
 
         $this->assertIsArray($year->work_days);
         $this->assertContainsOnly('int', $year->work_days);
+        $this->assertArrayHasKey('monday', $year->work_days);
+        $this->assertArrayHasKey('tuesday', $year->work_days);
+        $this->assertArrayHasKey('wednesday', $year->work_days);
+        $this->assertArrayHasKey('thursday', $year->work_days);
+        $this->assertArrayHasKey('friday', $year->work_days);
+        $this->assertArrayNotHasKey('saturday', $year->work_days);
+        $this->assertArrayNotHasKey('sunday', $year->work_days);
     }
 }
