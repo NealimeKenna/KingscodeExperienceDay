e914e9d7e7f73e0638d8e8f1ae6037f887c4af1a
Tom 'Neal <tomborneman@livewallgroup.com>
2023-05-16 10:52:09 +0200

    feature: make a function that calculates the amount of holidays per weekday and saves them.

--- app/Models/NationalHolidayYear.php
+++ app/Models/NationalHolidayYear.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use DateInterVal;
+use DateTime;
 use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
+use function easter_date;
 
 /**
  * App\Models\NationalHolidayYear
@@ -39,5 +42,84 @@ use Illuminate\Support\Carbon;
  */
 class NationalHolidayYear extends Model
 {
+    protected $fillable = ['year'];
 
+    protected static function boot()
+    {
+        self::created(function (NationalHolidayYear $year) {
+            $year->calculateHolidays();
+        });
+
+        parent::boot();
+    }
+
+    /**
+     * Function to calculate the amount of national holidays per weekday.
+     *
+     * For now this function is based on simple code provided by: https://gist.github.com/tvlooy/1894247
+     *
+     * National holidays can change so this should be replaced by something like:
+     * https://www.reinder.eu/nationale-feestdagen-api-heel-europa/
+     *
+     * But for some reason the promised api key never arrived in my mailbox (Yes I checked spam).
+     */
+    public function calculateHolidays()
+    {
+        $dates = collect();
+
+        // Set days
+        $dates->add(new DateTime("$this->year-01-01")); // New year
+        $dates->add(new DateTime("$this->year-05-05")); // Liberation day
+        $dates->add(new DateTime("$this->year-12-25")); // Christmas
+        $dates->add(new DateTime("$this->year-12-26")); // Boxing day (Christmas second day)
+
+        $monarchDay = new DateTime("$this->year-04-27");
+
+        // If the birthday of our monarch is on a sunday, it's moved to the saturday before.
+        if ($monarchDay->format('w') === '0') {
+            $monarchDay->sub(new DateInterval('P1D'));
+        }
+
+        $dates->add($monarchDay);
+
+        // Calculated holidays
+        $easter = new DateTime();
+        $easter->setTimestamp(easter_date($this->year)); // Use PHP's easter function
+
+        $dates->add($easter);
+
+        $easterMonday = clone $easter;
+        $easterMonday->add(new DateInterVal('P1D')); // 1 day after Easter
+
+        $dates->add($easterMonday);
+
+        $goodFriday = clone $easter;
+        $goodFriday->sub(new DateInterval('P2D')); // 2 days before Easter
+
+        $dates->add($goodFriday);
+
+        $ascensionDay = clone $easter;
+        $ascensionDay->add(new DateInterVal('P39D')); // 39 days after Easter
+
+        $dates->add($ascensionDay);
+
+        $whitSunday = clone $ascensionDay;
+        $whitSunday->add(new DateInterVal('P10D')); // 10 days after Ascension day
+
+        $dates->add($whitSunday);
+
+        $whitMonday = clone $whitSunday;
+        $whitMonday->add(new DateInterVal('P1D')); // 1 day after whit sunday
+
+        $dates->add($whitMonday);
+
+        $dates->each(function (DateTime $date) {
+            // I'm get the textual weekday of the date and add 1 to that day of the year.
+            $weekday = strtolower($date->format('l'));
+
+            $this->$weekday++;
+        });
+
+        $this->save();
+    }
 }
